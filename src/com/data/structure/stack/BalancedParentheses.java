faab4db6f479889e04f91c6bc21a3826ca266a66
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-23 22:13:31 +0530

    Balanced Parentheses :POP

--- src/com/data/structure/stack/BalancedParentheses.java
+++ src/com/data/structure/stack/BalancedParentheses.java
@@ -1,26 +1,43 @@
 package com.data.structure.stack;
 
 //Java program for checking Balanced Parentheses
-
-
 public class BalancedParentheses 
 {
- static class stack 
- {
-     int top=-1;
-     char items[] = new char[50];
+	static class stack 
+	{
+		int top=-1;
+		char items[] = new char[50];
 
-     void push(char x) 
-     {
-         if (top == 49) 
-         {
-             System.out.println("Stack Overflow");
-         } 
-         else
-         {
-             items[++top] = x;
-         }
-     }
+		void push(char x) 
+		{
+			if (top == 49) 
+			{
+				System.out.println("Stack Overflow");
+			} 
+			else
+			{
+				items[++top] = x;
+			}
+		}
+		char pop() 
+		{
+			if (top == -1) 
+			{
+				System.out.println("Error");
+				return '\0';
+			} 
+			else
+			{
+				char element = items[top];
+				top--;
+				return element;
+			}
+		}
+		//Return true if Stack Empty 
+		boolean isEmpty() 
+		{
+			return (top == -1) ? true : false;
+		}
+	}
 
- }
 }
