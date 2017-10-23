6fe15b7694c60c1566f1408d22730d65ca800394
imvishalk28 <imStayHungryStayFoolish@gmail.com>
2017-10-23 22:27:15 +0530

    Balanced parenthesis

--- src/com/data/structure/stack/BalancedParentheses.java
+++ src/com/data/structure/stack/BalancedParentheses.java
@@ -41,24 +41,61 @@ public class BalancedParentheses
 	}
 	// Check and return true, if Parentheses are in pair with open n close.
 	static boolean isInPair(char char1, char char2)
-    {
-       if (char1 == '(' && char2 == ')'){
-         return true;
-       }
-       else if (char1 == '{' && char2 == '}'){
-         return true;
-       }
-       else if (char1 == '[' && char2 == ']'){
-         return true;
-       }
-       else{
-         return false;
-       }
-    }
-	
-	
-	
-	
-	
+	{
+		if (char1 == '(' && char2 == ')'){
+			return true;
+		}
+		else if (char1 == '{' && char2 == '}'){
+			return true;
+		}
+		else if (char1 == '[' && char2 == ']'){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	// Return true for balanced
+	static boolean isBalancedParenthesis(char exp[])
+	{
+
+		stack stack=new stack();
+		for(int i=0;i<exp.length;i++)
+		{
+			// Push if Open parenthesis occurs
+			if (exp[i] == '{' || exp[i] == '(' || exp[i] == '['){
+				stack.push(exp[i]);
+			}
+			// Pop if close parenthesis occurs
+			if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']')
+			{
+				if (stack.isEmpty())
+				{
+					return false;
+				} 
+
+				/* isInPair return false if parenthesis does not has close and return false */
+				else if ( !isInPair(stack.pop(), exp[i]) )
+				{
+					return false;
+				}
+			}
+
+		}
+
+		//If stack empty then its balanced 
+		if (stack.isEmpty()){
+			return true;
+		}
+		else
+		{   //For unbalanced 
+			return false;
+		} 
+	} 
+
+
+
+
+
 
 }
